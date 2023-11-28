295f72a3d778e9cc731572a91f7cf167fe419913
Duong Le <duole93@live.com>
2023-11-27 20:32:35 -0600

    add operators, equal, reset functionality
    - add event listener to operator to set the operator and ready to accept
    the input of the second operand
    - add event listener to equal to get the result of the operations
    - add event listener to reset to reset the display

--- js/script.js
+++ js/script.js
@@ -1,38 +1,78 @@
 const numbers = document.querySelectorAll(".numPad button:nth-child(-n+10)");
 const decimal = document.querySelector(".btn-decimal");
 
-const op_plus = document.querySelector(".btn-op-plus");
-const op_minus = document.querySelector(".btn-op-minus");
-const op_multi = document.querySelector(".btn-op-multi");
-const op_division = document.querySelector(".btn-op-division");
+const operators = document.querySelectorAll('.operator button:nth-child(-n+4)');
 const equal = document.querySelector(".btn-equal");
 
-const reset = document.querySelector("btn-reset");
-const percent = document.querySelector("btn-percent");
-const negate = document.querySelector("btn-negate");
+const reset = document.querySelector(".btn-reset");
+const percent = document.querySelector(".btn-percent");
+const negate = document.querySelector(".btn-negate");
 
 const display = document.querySelector(".display");
 
 let ans = 0;
 let operand1 = 0;
 let operand2 = 0;
+let operator = '';
 let isDecimal = false;
-let operationBegin;
+
+//fa
+let isInputAccept=false;
+
+//display reset and accept new number input
+function displayInputReset(){
+    isInputAccept=true;
+    display.textContent = '';
+}
+
+//reset
+reset.addEventListener("click", ()=>{
+    display.textContent = '0';
+    isInputAccept=false;
+    isDecimal = false;
+})
 
 //display number
 numbers.forEach((num) => {
 	num.addEventListener("click", (event) => {
-        if(display.textContent==='0')
-            display.textContent='';
+        if (!isInputAccept){
+            displayInputReset();
+        }
 		display.textContent += num.textContent;
 	});
 
 });
+
 decimal.addEventListener('click',()=>{
-    if(isDecimal===false){
+    //when display is empty
+    if(!isInputAccept){
         display.textContent += '.';
-        isDecimal=true;
+        isInputAccept=true;
     }
+    //when display is not empty
+    else{
+        if(isDecimal===false){
+            display.textContent += '.';
+        }
+    }
+    isDecimal=true;
+})
+
+//operator
+operators.forEach((op)=>{
+    op.addEventListener("click", (event)=>{
+        operator = op.textContent;
+        operand1=parseInt(display.textContent);
+        isInputAccept=false;
+    })
+})
+
+//equal
+equal.addEventListener('click', ()=>{
+    operand2=parseInt(display.textContent);
+    ans = operation(operand1, operand2,operator);
+    display.textContent = ans;
+    isInputAccept=false;
 })
 
 
